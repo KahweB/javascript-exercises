793c51916ca2729e980f954bff3acd4d93d50c61
Kahve <72060765+KahweB@users.noreply.github.com>
2023-01-08 04:40:39 +0300

    01 - 02 test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, ntime) {
+    let output = "";
+    if (ntime < 0 ){
+        return "ERROR";
+    }
+    for(let i = 0; i < ntime; i++){
+        output += word;
+    }
+    return output;
 };
 
 // Do not edit below this line
