f2892b413e05418209ce920cf743ba92447a7dc0
Kahve <72060765+KahweB@users.noreply.github.com>
2023-01-09 02:48:24 +0300

    04 test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...args) {
+    //cikarilacak sayilari indexof a koy splice et ne kadar arguman konulduysa
+    // o kadar loopla (sadece 1 item splice edilecek)
+    
+    for (let i=0; i <= args.length -1 ;i++){
+        if(typeof array[array.indexOf(args[i])] === typeof args[i]){
+            array.splice(array.indexOf(args[i]),1);
+        }
+        else{
+            continue;
+        }
+    };  
+    return array;
 };
 
 // Do not edit below this line
