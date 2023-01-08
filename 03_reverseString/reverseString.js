200b95fcd3bd76af432764cc08e76edfbe68f945
Kahve <72060765+KahweB@users.noreply.github.com>
2023-01-08 04:55:59 +0300

    test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(normalString) {
+    let reversedString = ""
+    for(let i = (normalString.length)-1; i >= 0; i--){
+        reversedString += normalString[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
