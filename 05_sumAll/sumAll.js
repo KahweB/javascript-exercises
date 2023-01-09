ccabe933629e7c2ecadf424814ee91d4d25364f7
Kahve <72060765+KahweB@users.noreply.github.com>
2023-01-09 03:25:03 +0300

    05 test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne,numberTwo) {
+    let sumOfAll = 0;
+    if((numberOne > 0 && numberTwo > 0)&&(typeof numberOne === typeof numberTwo)){
+        if(numberOne < numberTwo){
+            for(let i= numberOne; i<=numberTwo;i++){
+                sumOfAll += i;
+            }
+        return sumOfAll;
+        }
+        else{
+            for(let i= numberTwo; i<=numberOne;i++){
+                sumOfAll += i;
+            }
+        return sumOfAll;
+        }
+    }
+    else{
+        return "ERROR"
+    }
 };
-
+sumAll();
 // Do not edit below this line
 module.exports = sumAll;
